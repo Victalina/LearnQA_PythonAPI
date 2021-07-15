a68766ced18df2c1d3fc368ac3a6a0413d63f2fa
Victalina <60832297+Victalina@users.noreply.github.com>
2021-07-15 17:22:36 +0300

    Ex16: Запрос данных другого пользователя

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,9 +1,12 @@
+import allure
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
+@allure.epic("Get user info cases")
 class TestUserGet(BaseCase):
+    @allure.description("This test get user info without authorization (only username)")
     def test_get_user_details_not_auth(self):
         response = MyRequests.get("/user/2")
 
@@ -12,6 +15,7 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_not_key(response, "lastName")
         Assertions.assert_json_has_not_key(response, "email")
 
+    @allure.description("This test get user info with authorization (all fields)")
     def test_get_user_details_auth_as_same_user(self):
         data = {
             'email':'vinkotov@example.com',
@@ -29,3 +33,32 @@ class TestUserGet(BaseCase):
                                    cookies={'auth_sid': auth_sid})
         expected_fields = ["username", "firstName", "lastName", "email"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    @allure.description("This test get user info with authorization with different user (only username)")
+    def test_get_user_details_auth_as_different_user(self):
+        # LOGIN WITH USER 1
+        data = {
+            'email':'vinkotov@example.com',
+            'password':'1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, 'auth_sid')
+        token = self.get_header(response1, 'x-csrf-token')
+
+        # CREATE USER 2
+        register_data = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=register_data)
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+        user_id_different = self.get_json_value(response2, "id")
+
+        # GET USER 2 WITH USER 1
+        response3 = MyRequests.get(f"/user/{user_id_different}",
+                                   headers={'x-csrf-token': token},
+                                   cookies={'auth_sid': auth_sid})
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_not_key(response3, "firstName")
+        Assertions.assert_json_has_not_key(response3, "lastName")
+        Assertions.assert_json_has_not_key(response3, "email")
